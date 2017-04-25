9ff2ed90bd734d33b2fdf8a5ef44f6caa87a81b9
Khuat Van Dung <dungkv95@gmail.com>
2017-04-25 10:16:59 +0700

    //PAssing data with segue and without segue

--- PassingData/ViewController.swift
+++ PassingData/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var text: String?
+    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.text = text 
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destinationVC = segue.destination as? DestinationViewController {
+            destinationVC.text = textField.text ?? "Nil"
+        }
+    }
+    @IBAction func passingDataWithoutSegue(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil) // Name of storyboard....
+        let destinationVC = storyboard.instantiateViewController(withIdentifier: "DestinationViewController") as! DestinationViewController
+        destinationVC.text = textField.text ?? "Nil"
+        self.present(destinationVC, animated: true, completion: nil)
+    }
 
 }
 
